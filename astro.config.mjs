5b00d0d4d3739a7814c048ae9ce47ee02e23ab26
Andrei Olaru <andrei.olaru@gmail.com>
2026-04-26 09:43:43 +0300

    Start Astro site migration

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://kreativsound.com",
+  output: "static",
+  trailingSlash: "always"
+});
---
+++ scripts/sync-astro-public.mjs
@@ -0,0 +1,60 @@
+import fs from "node:fs";
+import path from "node:path";
+
+const rootDir = process.cwd();
+const publicDir = path.join(rootDir, "public");
+
+const filesToCopy = [
+  "favicon.svg",
+  "favicon-16x16.png",
+  "favicon-32x32.png",
+  "apple-touch-icon.png",
+  "logo-128.svg",
+  "og-image.svg",
+  "site.js",
+  "share.js",
+  "main.js",
+  "products.js",
+  "robots.txt",
+  "sitemap.xml",
+  "CNAME",
+  "404.html",
+  "about.html",
+  "contact.html",
+  "learn.html",
+  "news.html",
+  "sound.html",
+  "tools.html"
+];
+
+const directoriesToCopy = [
+  "about",
+  "apps",
+  "assets",
+  "contact",
+  "learn",
+  "news",
+  "posts",
+  "products"
+];
+
+function resetDirectory(targetDir) {
+  fs.rmSync(targetDir, { recursive: true, force: true });
+  fs.mkdirSync(targetDir, { recursive: true });
+}
+
+function copyEntry(relativePath) {
+  const sourcePath = path.join(rootDir, relativePath);
+  const targetPath = path.join(publicDir, relativePath);
+  fs.cpSync(sourcePath, targetPath, { recursive: true });
+}
+
+resetDirectory(publicDir);
+
+for (const filePath of filesToCopy) {
+  copyEntry(filePath);
+}
+
+for (const directoryPath of directoriesToCopy) {
+  copyEntry(directoryPath);
+}
